aa4e51aee75d6e9c1a12e99f424e4563ffbccac2
Maya García <mayagar6@gmail.com>
2023-11-02 07:23:50 -0400

    Final project finished

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaPlusSquare } from 'react-icons/fa';
+import { FaMinusSquare } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -25,11 +27,24 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name)  => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaPlusSquare size='2.2em' color="green" onClick={event=> increaseAllocation(props.name)}></FaPlusSquare></td>
+        <td><FaMinusSquare size='2.2em' color="blue" onClick={event=> decreaseAllocation(props.name)}></FaMinusSquare></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
